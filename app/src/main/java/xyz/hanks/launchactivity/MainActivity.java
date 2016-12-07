becb491942132cad5ed4538ab69ef9df9f61ccd1
hanks-zyh <zhangyuhan2014@gmail.com>
2016-12-08 00:25:09 +0800

    MainActivity change to GridLayout

--- app/src/main/java/xyz/hanks/launchactivity/MainActivity.java
+++ app/src/main/java/xyz/hanks/launchactivity/MainActivity.java
@@ -1,22 +1,18 @@
 package xyz.hanks.launchactivity;
 
-import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import net.dongliu.apk.parser.ApkParser;
 
@@ -26,62 +22,21 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import xyz.hanks.launchactivity.model.ApkInfo;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.action) EditText etAction;
-    @BindView(R.id.category) EditText etCategory;
-    @BindView(R.id.data) EditText etData;
     @BindView(R.id.recyclerView) RecyclerView recyclerView;
 
     private AppInfoAdapter adapter;
     private List<ApkInfo> data = new ArrayList<>();
-
-    @OnClick(R.id.btn_launch)
-    public void launch() {
-        try {
-            String actions = etAction.getText().toString();
-            String catagories = etCategory.getText().toString();
-            String datas = etData.getText().toString();
-
-            Intent intent = new Intent();
-
-            if (!TextUtils.isEmpty(actions)) {
-                intent.setAction(actions);
-            }
-            if (!TextUtils.isEmpty(catagories)) {
-                for (String line : catagories.split("#")) {
-                    intent.addCategory(line);
-                }
-            }
-            if (!TextUtils.isEmpty(datas)) {
-                intent.setData(Uri.parse(datas));
-            }
-
-            PackageManager packageManager = getPackageManager();
-            List activities = packageManager.queryIntentActivities(intent,
-                    PackageManager.MATCH_DEFAULT_ONLY);
-            boolean isIntentSafe = activities.size() > 0;
-
-            if (!isIntentSafe) {
-                Toast.makeText(this, "未找到", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            startActivity(intent);
-        } catch (Exception e) {
-            Toast.makeText(this, "error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
         adapter = new AppInfoAdapter();
         recyclerView.setAdapter(adapter);
         getData();
@@ -143,7 +98,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(AppInfoHolder holder, int position) {
-            holder.tvInfo.setText(data.get(position).getAppName());
+            holder.ivIcon.setImageDrawable(data.get(position).getIcon());
+            holder.tvName.setText(data.get(position).getAppName());
         }
 
         @Override
@@ -154,12 +110,14 @@ public class MainActivity extends AppCompatActivity {
 
     public class AppInfoHolder extends RecyclerView.ViewHolder {
 
-        private final TextView tvInfo;
+        private final TextView tvName;
+        private final ImageView ivIcon;
 
         public AppInfoHolder(View itemView) {
             super(itemView);
-            tvInfo = (TextView) itemView.findViewById(R.id.tv_info);
-            tvInfo.setOnClickListener(new View.OnClickListener() {
+            tvName = (TextView) itemView.findViewById(R.id.tv_name);
+            ivIcon = (ImageView) itemView.findViewById(R.id.iv_icon);
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     XmlSourceViewerActivity.start(MainActivity.this, data.get(getAdapterPosition()).getPath());
